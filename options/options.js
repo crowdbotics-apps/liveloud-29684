d5f06512f98cac8b6a3f3e4450d33185fb96c071
Crowdbotics <team@crowdbotics.com>
2021-08-10 01:59:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "liveloud_29684Identifier",
+  url: "https://liveloud_29684Identifier.botics.co",
+  api: "https://liveloud_29684Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
